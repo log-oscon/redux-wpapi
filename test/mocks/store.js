ea8dbe42b96a840508dbc7f37555b711d6d9ec63
Edygar de Lima Oliveira <edygar@me.com>
2016-09-14 10:26:59 +0100

    Fixing selector

--- test/mocks/store.js
+++ test/mocks/store.js
@@ -3,7 +3,6 @@ import { initialReducerState } from '../../src/ReduxWPAPI';
 const initialStore = { wp: initialReducerState };
 export const createFakeStore = (fakeData = initialStore) => ({ getState: () => fakeData });
 
-
 // export const dispatchWithStoreOf = (storeData, action) =>
 //   new Promise((resolve, reject) => {
 //     x
