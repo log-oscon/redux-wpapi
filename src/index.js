6fae2e0badf60b62ef4bc874209fcd8c5d2a7917
Edygar de Lima Oliveira <edygar@me.com>
2016-09-19 11:23:14 +0100

    Exports Symbols at root

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReduxWPAPI from './ReduxWPAPI';
 export default ReduxWPAPI;
 export * as RequestStatus from './constants/requestStatus';
 export callAPI from './actions/callAPI';
+export * as Symbols from './symbols';
 export { selectQuery } from './selectors';
