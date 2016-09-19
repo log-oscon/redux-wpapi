e0abc57b1258d4fe10eea7f3d5e83a58164ae145
Edygar de Lima Oliveira <edygar@me.com>
2016-09-19 11:29:45 +0100

    Exposes withDenormalize

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ export default ReduxWPAPI;
 export * as RequestStatus from './constants/requestStatus';
 export callAPI from './actions/callAPI';
 export * as Symbols from './symbols';
-export { selectQuery } from './selectors';
+export { selectQuery, withDenormalize } from './selectors';
